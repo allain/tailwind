40bc8498d15d32a7d69283226470a13d1c43195f
allain <allain.lalonde@gmail.com>
2021-03-08 16:17:18 -0500

    wip more tweaks on npm install

--- scripts/build-all.bash
+++ scripts/build-all.bash
@@ -15,7 +15,7 @@ for platform in "${platforms[@]}" ; do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name=$package_name'-'$version'-'$GOOS'-'$GOARCH
+    output_name=$package_name'-v'$version'-'$GOOS'-'$GOARCH
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
